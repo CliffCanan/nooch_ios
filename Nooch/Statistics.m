687e64d36db17a57b41432ae8267768330ab40e5
Cliff Canan <cliff@nooch.com>
2014-04-14 22:44:21 +0000

    • Removed Stats for Deposits/Withdrawals on the First "Profile Stats" Table

--- Nooch/Statistics.m
+++ Nooch/Statistics.m
@@ -1,10 +1,8 @@
-//
 //  Statistics.m
 //  Nooch
 //
 //  Created by crks on 10/7/13.
-//  Copyright (c) 2013 Nooch. All rights reserved.
-//
+//  Copyright (c) 2014 Nooch. All rights reserved.
 
 #import "Statistics.h"
 #import "Home.h"
@@ -35,7 +33,6 @@
 {
     [super viewDidLoad];
     self.navigationController.navigationBar.topItem.title = @"";
-    
     [self.navigationItem setHidesBackButton:YES];
     [self.navigationController.view removeGestureRecognizer:self.navigationController.slidingViewController.panGesture];
     [self.view setStyleClass:@"background_gray"];
@@ -51,57 +48,57 @@
     [hamburger setBackgroundImage:ttt.image forState:UIControlStateNormal];
     UIBarButtonItem *menu = [[UIBarButtonItem alloc] initWithCustomView:hamburger];
     [self.navigationItem setLeftBarButtonItem:menu];
-    
+
     serve*serveOBJ=[serve new];
     [serveOBJ setDelegate:self];
     serveOBJ.tagName=@"Total_P2P_transfers";
     [serveOBJ GetMemberStats:@"Total_P2P_transfers"];
 	// Do any additional setup after loading the view.
     [self.navigationItem setTitle:@"Statistics"];
-    
+
     self.selected = 1;
     UISwipeGestureRecognizer *left = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(change_stats:)];
     [left setDirection:UISwipeGestureRecognizerDirectionLeft];
     [self.view addGestureRecognizer:left];
-    
+
     UISwipeGestureRecognizer *right = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(change_stats:)];
     [right setDirection:UISwipeGestureRecognizerDirectionRight];
     [self.view addGestureRecognizer:right];
-    
+
     self.back = [UIView new];
     [self.back setBackgroundColor:[UIColor whiteColor]];
     [self.back setFrame:CGRectMake(10, 10, 300, 400)];
     [self.back setStyleClass:@"raised_view"];
     [self.view addSubview:self.back];
-    
+
     self.profile = [UIImageView new];
     [self.profile setStyleClass:@"stats_circle"];
     [self.profile setStyleId:@"stats_circle_profile_inactive"];
     [self.back addSubview:self.profile];
-    
+
     self.transfers = [UIImageView new];
     [self.transfers setStyleClass:@"stats_circle"];
     [self.transfers setStyleId:@"stats_circle_transfers_active"];
     [self.back addSubview:self.transfers];
-    
+
     self.donations = [UIImageView new];
     [self.donations setStyleClass:@"stats_circle"];
     [self.donations setStyleId:@"stats_circle_donations_inactive"];
     [self.back addSubview:self.donations];
-    
+
     self.header = [UILabel new];
     [self.header setText:@"Profile Stats"];
     [self.header setStyleClass:@"stats_header"];
     [self.back addSubview:self.header];
-    
+
     self.stats = [UITableView new];
     [self.stats setDelegate:self]; [self.stats setDataSource:self];
     [self.stats setStyleClass:@"stats"];
     [self.back addSubview:self.stats];
     [self.stats setUserInteractionEnabled:NO];
     [self.stats reloadData];
-    
-    blankView=[[UIView alloc]initWithFrame:CGRectMake(0, 0,320, self.view.frame.size.height)];
+
+    blankView=[[UIView alloc]initWithFrame:CGRectMake(0, 0, 320, self.view.frame.size.height)];
     [blankView setBackgroundColor:[UIColor colorWithRed:0 green:0 blue:0 alpha:0.6]];
     UIActivityIndicatorView*actv=[[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhiteLarge];
     [actv setFrame:CGRectMake(140,(self.view.frame.size.height/2)-5, 40, 40)];
@@ -111,15 +108,13 @@
     [self.view bringSubviewToFront:blankView];
 }
 
-- (void) button_change:(int)num {
-    
-}
+- (void) button_change:(int)num {}
 
 - (void) change_stats:(UISwipeGestureRecognizer *)slide
 {
     [UIView beginAnimations:nil context:nil];
     CGRect frame = self.back.frame;
-    [UIView setAnimationDuration:0.5];
+    [UIView setAnimationDuration:0.4];
     if (slide.direction == UISwipeGestureRecognizerDirectionLeft) {
         if (self.selected == 0) {
             self.selected++;
@@ -174,7 +169,7 @@
     frame.origin.x = frame.origin.x * -1;
     self.back.frame = frame;
     [UIView beginAnimations:nil context:nil];
-    [UIView setAnimationDuration:0.5];
+    [UIView setAnimationDuration:0.4];
     frame.origin.x = 10;
     [self.back setFrame:frame];
     [UIView commitAnimations];
@@ -188,8 +183,7 @@
     [Title setStyleClass:@"titlelbl"];
     Title.text=titlestr;
     [view addSubview:Title];
-    return view;
-    
+    return view;    
 }
 -(CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section{
     return 25;
@@ -205,54 +199,32 @@
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
     
     if (cell == nil) {
-        
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault
                                       reuseIdentifier:CellIdentifier];
-        
         [cell.textLabel setTextColor:kNoochGrayLight];
     }
-    
     if ([cell.contentView subviews]){
         for (UIView *subview in [cell.contentView subviews]) {
             [subview removeFromSuperview];
         }
     }
-    
+
     UILabel *title = [UILabel new];
     UILabel *statistic = [UILabel new];
     [title setStyleClass:@"stats_table_left_lable"];
     [statistic setStyleClass:@"stats_table_right_lable"];
-    
-    if (self.selected == 0) { //profile
+
+    if (self.selected == 0) {
         if (indexPath.row == 0) {
-            [title setText:@"$ Added to Nooch"];
-            //[statistic setText:@"$ 105.00"];
-            [statistic setText:[[dictAllStats valueForKey:@"Total_$_Added_to_Nooch"]  valueForKey:@"Result"]];
-            if ([[[dictAllStats valueForKey:@"Total_$_Added_to_Nooch"]valueForKey:@"Result"] length]==0) {
-                [statistic setText:@"0"];
-            }
-            //Total_$_Added_to_Nooch
-        }
-        else if (indexPath.row == 1) {
-            [title setText:@"$ Cashed out of Nooch"];
-            //[statistic setText:@"$ 200.00"];
-            [statistic setText:[[dictAllStats valueForKey:@"Total_$_withdraw_from_Nooch"]  valueForKey:@"Result"]];
-            if ([[[dictAllStats valueForKey:@"Total_$_withdraw_from_Nooch" ]valueForKey:@"Result"] length]==0) {
-                [statistic setText:@"0"];
-            }
-            //Total_$_withdraw_from_Nooch
-        }
-        else if (indexPath.row == 2) {
-            [title setText:@"Friends Invited"];
+           [title setText:@"Friends Invited"];
             if ([dictAllStats valueForKey:@"Total_Friends_Invited"]) {
                 [statistic setText:[[dictAllStats valueForKey:@"Total_Friends_Invited"]  valueForKey:@"Result"]];
             }
             if ([[[dictAllStats valueForKey:@"Total_Friends_Invited"]valueForKey:@"Result"] length]==0) {
                 [statistic setText:@"0"];
             }
-            // [statistic setText:@"4"];
         }
-        else if (indexPath.row == 3) {
+        else if (indexPath.row == 1) {
             [title setText:@"Invites Accepted"];
             if ([dictAllStats valueForKey:@"Total_Friends_Joined"]) {
                 [statistic setText:[[dictAllStats valueForKey:@"Total_Friends_Joined"]  valueForKey:@"Result"]];
@@ -260,10 +232,8 @@
             if ([[[dictAllStats valueForKey:@"Total_Friends_Joined"]valueForKey:@"Result"] length]==0) {
                 [statistic setText:@"0"];
             }
-            // [statistic setText:@"7"];
         }
-        
-        else if (indexPath.row == 4) {
+        else if (indexPath.row == 2) {
             if ([dictAllStats valueForKey:@"Total_Posts_To_TW"]) {
                 [statistic setText:[[dictAllStats valueForKey:@"Total_Posts_To_TW"]  valueForKey:@"Result"]];
             }
@@ -271,9 +241,8 @@
                 [statistic setText:@"0"];
             }
             [title setText:@"Posts to Twitter"];
-            // [statistic setText:@"0"];
         }
-        else if (indexPath.row == 5) {
+        else if (indexPath.row == 3) {
             if ([dictAllStats valueForKey:@"Total_Posts_To_FB"]) {
                 [statistic setText:[[dictAllStats valueForKey:@"Total_Posts_To_FB"]  valueForKey:@"Result"]];
             }
@@ -281,22 +250,19 @@
                 [statistic setText:@"0"];
             }
             [title setText:@"Posts to Facebook"];
-            // [statistic setText:@"3"];
         }
-    } else if (self.selected == 1) { //transfers
-        
-        
+    } 
+
+    else if (self.selected == 1) { //transfers
+
         if (indexPath.row == 0) {
             [title setText:@"Total # of Transfers"];
-            //[statistic setText:@"27"];
             if ([dictAllStats valueForKey:@"Total_P2P_transfers"]) {
                 [statistic setText:[[dictAllStats valueForKey:@"Total_P2P_transfers"]  valueForKey:@"Result"]];
             }
             if ([[[dictAllStats valueForKey:@"Total_P2P_transfers"]valueForKey:@"Result"] length]==0) {
                 [statistic setText:@"0"];
             }
-            
-            //
         }
         else if (indexPath.row == 1) {
             [title setText:@"Transfers Sent"];
@@ -304,7 +270,6 @@
             if ([[[dictAllStats valueForKey:@"Total_no_of_transfer_Sent"]valueForKey:@"Result"] length]==0) {
                 [statistic setText:@"0"];
             }
-            
         }
         else if (indexPath.row == 2) {
             [title setText:@"Transfers Received"];
@@ -312,7 +277,6 @@
             if ([[[dictAllStats valueForKey:@"Total_no_of_transfer_Received"]valueForKey:@"Result"] length]==0) {
                 [statistic setText:@"0"];
             }
-            
         }
         else if (indexPath.row == 3) {
             [title setText:@"$ Amount Sent"];
@@ -320,11 +284,9 @@
             if ([[[dictAllStats valueForKey:@"Total_$_Sent"]valueForKey:@"Result"] length]==0) {
                 [statistic setText:@"0"];
             }
-            
         }
         else if (indexPath.row == 4) {
             [title setText:@"$ Amount Received"];
-            
             [statistic setText:[[dictAllStats valueForKey:@"Total_$_Received"] valueForKey:@"Result"]];
             if ([[[dictAllStats valueForKey:@"Total_$_Received"]valueForKey:@"Result"] length]==0) {
                 [statistic setText:@"0"];
@@ -332,33 +294,27 @@
         }
         else if (indexPath.row == 5) {
             [title setText:@"Largest Transfer Sent"];
-            
             [statistic setText:[[dictAllStats valueForKey:@"Largest_sent_transfer"]  valueForKey:@"Result"]];
             if ([[[dictAllStats valueForKey:@"Largest_sent_transfer"]valueForKey:@"Result"] length]==0) {
                 [statistic setText:@"0"];
             }
-            
         }
         else if (indexPath.row == 6) {
-            
-            
             [title setText:@"Largest Transfer Received"];
             [statistic setText:[[dictAllStats valueForKey:@"Largest_received_transfer"]  valueForKey:@"Result"]];
             if ([[[dictAllStats valueForKey:@"Largest_received_transfer"]valueForKey:@"Result"] length]==0) {
                 [statistic setText:@"0"];
             }
-            
         }
-    } else if (self.selected == 2) { //donations
-        
-        
+    } 
+
+    else if (self.selected == 2) { //donations
         if (indexPath.row == 0) {
             [title setText:@"Total $ Donated"];
             [statistic setText:[[dictAllStats valueForKey:@"Total_$_Donated"]  valueForKey:@"Result"]];
             if ([[[dictAllStats valueForKey:@"Total_$_Donated"]valueForKey:@"Result"] length]==0) {
                 [statistic setText:@"0"];
             }
-            //[statistic setText:@"$ 105.00"];
         }
         else if (indexPath.row == 1) {
             [title setText:@"Total Donations"];
@@ -366,7 +322,6 @@
             if ([[[dictAllStats valueForKey:@"Total_Donations_Count"]valueForKey:@"Result"] length]==0) {
                 [statistic setText:@"0"];
             }
-            
         }
         else if (indexPath.row == 2) {
             [title setText:@"Causes Donated to"];
@@ -382,10 +337,9 @@
             if ([[[dictAllStats valueForKey:@"Largest_Donation_Made"]valueForKey:@"Result"] length]==0) {
                 [statistic setText:@"0"];
             }
-            
         }
     }
-    
+
     [cell.contentView addSubview:title];
     [cell.contentView addSubview:statistic];
     return cell;
@@ -471,7 +425,6 @@
         serveOBJ.tagName=@"Total_$_withdraw_from_Nooch";
         [serveOBJ GetMemberStats:@"Total_$_withdraw_from_Nooch"];
     }
-    
     else if ([tagName isEqualToString:@"Total_$_withdraw_from_Nooch"]) {
         serve*serveOBJ=[serve new];
         [serveOBJ setDelegate:self];
@@ -524,7 +477,6 @@
         [blankView removeFromSuperview];
         [self.stats reloadData];
     }
-    //DonatedTo
 }
 
 - (void)viewWillDisappear:(BOOL)animated
@@ -537,5 +489,4 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
 @end
