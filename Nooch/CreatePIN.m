8ad610aeb80e32755a8cec9b207273f41fe0009b
Cliff Canan <cliff@nooch.com>
2014-04-15 01:31:00 +0000

    • Edited instruction text to remove reference to withdrawals

--- Nooch/CreatePIN.m
+++ Nooch/CreatePIN.m
@@ -1,10 +1,8 @@
-//
 //  CreatePIN.m
 //  Nooch
 //
 //  Created by crks on 10/1/13.
-//  Copyright (c) 2013 Nooch. All rights reserved.
-//
+//  Copyright (c) 2014 Nooch. All rights reserved.
 
 #import "CreatePIN.h"
 #import "Home.h"
@@ -47,8 +45,7 @@
 #pragma mark - UITextField delegation
 - (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string{
     int len = [textField.text length] + [string length];
-    if([string length] == 0) //deleting
-    {
+    if([string length] == 0) //deleting {
         switch (len) {
             case 4:
                 [self.fourth_num setBackgroundColor:[UIColor clearColor]];
@@ -67,7 +64,8 @@
             default:
                 break;
         }
-    }else{
+    }
+    else{
         UIColor *which = kNoochGreen;
         switch (len) {
             case 5:
@@ -84,11 +82,11 @@
                     [self.third_num setBackgroundColor:[UIColor clearColor]];
                     [self.fourth_num setBackgroundColor:[UIColor clearColor]];
                     return NO;
-                }else{
+                }
+                else{
                     if ([self.pin_check isEqualToString:[NSString stringWithFormat:@"%@%@",textField.text,string]]) {
                         [self.user setObject:[NSString stringWithFormat:@"%@%@",textField.text,string] forKey:@"pin_number"];
                         //push invite code
-                        
                         ReferralCode *code_entry = [[ReferralCode alloc] initWithData:self.user];
                         [self.navigationController pushViewController:code_entry animated:YES];
                     }else{
@@ -123,7 +121,6 @@
 -(void) BackClicked:(id) sender
 
 {
-    
     [self.navigationController popViewControllerAnimated:YES];
     
 }
@@ -135,51 +132,49 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    
     UIButton* btnback=[UIButton buttonWithType:UIButtonTypeCustom];
-    
+
     [btnback setImage:[UIImage imageNamed:@"back-arrow-blue.png"] forState:UIControlStateNormal];
     [btnback setStyleClass:@"back_button-icon"];
     btnback.frame=CGRectMake(0, 7, 50, 30);
-    
     [btnback addTarget:self action:@selector(BackClicked:) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:btnback];
     self.pin_check = @"";
     [self.view setBackgroundColor:[UIColor whiteColor]];
-    
+
     UIImageView *logo = [UIImageView new];
     [logo setStyleId:@"prelogin_logo"];
     [self.view addSubview:logo];
-    
+
     [self.navigationItem setTitle:@"Create PIN"];
-    
+
     UILabel *title = [[UILabel alloc] initWithFrame:CGRectMake(10, 140, 300, 40)];
     [title setText:@"Create your PIN"];
     [title setStyleClass:@"header_signupflow"];
     [self.view addSubview:title];
-    
+
     self.prompt = [[UILabel alloc] initWithFrame:CGRectMake(20, 160, 280, 50)];
     [self.prompt setNumberOfLines:2];
-    [self.prompt setText:@"You'll be asked to enter this PIN anytime you send, withdraw, or deposit money."];
+    [self.prompt setText:@"You'll be asked to enter this PIN anytime you send or request money."];
     [self.prompt setStyleClass:@"instruction_text"];
     [self.view addSubview:self.prompt];
     
     self.pin = [UITextField new]; [self.pin setKeyboardType:UIKeyboardTypeNumberPad];
     [self.pin setDelegate:self]; [self.pin setFrame:CGRectMake(800, 800, 20, 20)];
     [self.view addSubview:self.pin]; [self.pin becomeFirstResponder];
-    
+
     self.first_num = [[UIView alloc] initWithFrame:CGRectMake(85,240,30,30)];
     self.second_num = [[UIView alloc] initWithFrame:CGRectMake(125,240,30,30)];
     self.third_num = [[UIView alloc] initWithFrame:CGRectMake(165,240,30,30)];
     self.fourth_num = [[UIView alloc] initWithFrame:CGRectMake(205,240,30,30)];
-    
+
     if ([[UIScreen mainScreen] bounds].size.height == 480) {
         self.first_num = [[UIView alloc] initWithFrame:CGRectMake(85,230,30,30)];
         self.second_num = [[UIView alloc] initWithFrame:CGRectMake(125,230,30,30)];
         self.third_num = [[UIView alloc] initWithFrame:CGRectMake(165,230,30,30)];
         self.fourth_num = [[UIView alloc] initWithFrame:CGRectMake(205,230,30,30)];
     }
-    
+
     //self.first_num.alpha = self.second_num.alpha = self.third_num.alpha = self.fourth_num.alpha = 0.5;
     self.first_num.layer.cornerRadius = self.second_num.layer.cornerRadius = self.third_num.layer.cornerRadius = self.fourth_num.layer.cornerRadius = 15;
     self.first_num.backgroundColor = self.second_num.backgroundColor = self.third_num.backgroundColor = self.fourth_num.backgroundColor = [UIColor clearColor];
