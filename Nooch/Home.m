5431a3a3e380ba6ca9f9e0b8cc2f5079bdaae525
baljeetSingh21 <baljeet.singh@venturepact.com>
2014-09-10 15:31:33 +0530

    uncomment checks

--- Nooch/Home.m
+++ Nooch/Home.m
@@ -1158,13 +1158,13 @@ void addressBookChanged(ABAddressBookRef addressBook, CFDictionaryRef info, void
         return;
     }
 
-//    if (![[defaults valueForKey:@"IsVerifiedPhone"]isEqualToString:@"YES"] )
-//    {
-//        UIAlertView*alert=[[UIAlertView alloc]initWithTitle:@"Blame The Lawyers" message:@"To keep Nooch safe, we ask all users to verify a phone number before before sending money.\n \n If you've already added your phone number, just respond 'Go' to the text message we sent." delegate:self cancelButtonTitle:@"Later" otherButtonTitles:@"Add Phone", nil];
-//        [alert setTag:148];
-//        [alert show];
-//        return;
-//    }
+    if (![[defaults valueForKey:@"IsVerifiedPhone"]isEqualToString:@"YES"] )
+    {
+        UIAlertView*alert=[[UIAlertView alloc]initWithTitle:@"Blame The Lawyers" message:@"To keep Nooch safe, we ask all users to verify a phone number before before sending money.\n \n If you've already added your phone number, just respond 'Go' to the text message we sent." delegate:self cancelButtonTitle:@"Later" otherButtonTitles:@"Add Phone", nil];
+        [alert setTag:148];
+        [alert show];
+        return;
+    }
 
     if ( ![[[NSUserDefaults standardUserDefaults] objectForKey:@"IsBankAvailable"]isEqualToString:@"1"])
     {
